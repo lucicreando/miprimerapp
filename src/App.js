66f9bdd13cec78cac20e1377c9a848c4a7815ae7
lucicreando <luicelibuenosaites2019@gmail.com>
2023-10-02 16:31:44 -0300

    cardenReact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Usuario from './componentes/Usuario';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Usuario color= "pink" nombre="Maria" cargo="Gerente general" conoceme="CV de Maria"/>
+      <Usuario color= "blue" nombre="Luis Garcia" cargo="Subgerente general" conoceme="CV de Luis"/>
+      <Usuario color= "yellow" nombre="Pedro" cargo="Gerente general de región" conoceme="CV de Pedro" />
     </div>
   );
 }
